3208cd35a849b3735acda7a0a67046fa7d81a6c3
AGD <aloeverdulay@live.com>
2020-10-20 12:51:23 +0800

    Updated contents

--- assemble.bat
+++ assemble.bat
@@ -2,4 +2,4 @@
 ::	.\install.bat
 ::)
 ::.\bin\builder.exe .\doc .\ME-Project.docx
-.\builder\src\builder.py .\doc .\ME-Project.docx
+python3 .\builder\src\builder.py .\doc .\ME-Project.docx
--- assemble.sh
+++ assemble.sh
@@ -2,4 +2,4 @@
 
 #[ ! -f ./bin/builder ] || mkdir ./bin/builder
 #./dist/builder ./doc ./ME-Project.docx
-./builder/src/builder.py ./doc ./ME-Project.docx
+python3 ./builder/src/builder.py ./doc ./ME-Project.docx
