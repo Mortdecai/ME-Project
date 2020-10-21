9a3c913ff644112fe74e7dd1b19e18ebf7263607
AGD <aloeverdulay@live.com>
2020-10-21 19:54:24 +0800

    Updated contents

---
+++ install-builder-req.bat
@@ -0,0 +1,5 @@
+py -m pip install wheel
+py -m pip install lxml
+py -m pip install python-docx
+py -m pip install docx2pdf
+py -m pip install docxcompose 
--- install-builder-reqs.bat
+++
@@ -1,5 +0,0 @@
-pip install wheel
-pip install lxml
-pip install python-docx
-pip install docx2pdf
-pip install docxcompose 
